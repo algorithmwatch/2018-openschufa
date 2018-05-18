f23e9d4dbe84b041293a18854c2d10f71193d3e2
Oliver (T560) <olli.bienert@gmail.com>
2018-05-18 10:27:06 +0200

    Backend: ping/ endpoint returns number of user records in database

--- backend/tests.py
+++ backend/tests.py
@@ -8,7 +8,7 @@ def test_ping(app):
     resp = client.get('/ping')
     data = json.loads(resp.data.decode())
     assert resp.status_code == 200
-    assert 'pong' in data['message']
+    assert 'records' in data['message']
     assert 'success' in data['status']
 
 

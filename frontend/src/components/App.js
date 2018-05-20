2853dbe5896873b31fc829608366fc5ab6ed6044
Johannes Filter <hi@jfilter.de>
2018-05-21 01:06:36 +0200

    add routes for modals (close #22)

--- frontend/src/components/App.js
+++ frontend/src/components/App.js
@@ -5,6 +5,8 @@ import Modal from './Modal/ModalContainer';
 import { MuiThemeProvider, createMuiTheme } from 'material-ui/styles';
 import CssBaseline from 'material-ui/CssBaseline';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import { openModal } from '../actions';
+import { connect } from 'react-redux';
 
 const theme = createMuiTheme({
   palette: {
@@ -37,6 +39,75 @@ theme.overrides = {
   },
 };
 
+// To get URLs to link to, we create dummy components that open the modal.
+// This is very hacky but because of the project setup, it's not easy to get
+// this done now...
+const SimpleGoBack = () => (
+  <div>
+    <a href="../">Go back</a>
+  </div>
+);
+
+class FaqPage extends Component {
+  componentDidMount() {
+    this.props.dispatch(openModal('faq'));
+  }
+
+  render() {
+    return <SimpleGoBack />;
+  }
+}
+
+const connectedFaqPage = connect()(FaqPage);
+
+class InformationPage extends Component {
+  componentDidMount() {
+    this.props.dispatch(openModal('openschufa'));
+  }
+
+  render() {
+    return <SimpleGoBack />;
+  }
+}
+
+const connectedInformationPage = connect()(InformationPage);
+
+class ImprintPage extends Component {
+  componentDidMount() {
+    this.props.dispatch(openModal('imprint'));
+  }
+
+  render() {
+    return <SimpleGoBack />;
+  }
+}
+
+const connectedImprintPage = connect()(ImprintPage);
+
+class PrivacyPage extends Component {
+  componentDidMount() {
+    this.props.dispatch(openModal('dataprivacy'));
+  }
+
+  render() {
+    return <SimpleGoBack />;
+  }
+}
+
+const connectedPrivacyPage = connect()(PrivacyPage);
+
+class PrivacyAgreementPage extends Component {
+  componentDidMount() {
+    this.props.dispatch(openModal('privacyagreement'));
+  }
+
+  render() {
+    return <SimpleGoBack />;
+  }
+}
+
+const connectedPrivacyAgreementPage = connect()(PrivacyAgreementPage);
+
 class App extends Component {
   render() {
     return (
@@ -46,6 +117,14 @@ class App extends Component {
           <Switch>
             <Route exact path="/" component={LandingPage} />
             <Route path="/steps" component={Stepper} />
+            <Route path="/faq" component={connectedFaqPage} />
+            <Route path="/information" component={connectedInformationPage} />
+            <Route path="/imprint" component={connectedImprintPage} />
+            <Route path="/privacy" component={connectedPrivacyPage} />
+            <Route
+              path="/privacyagreement"
+              component={connectedPrivacyAgreementPage}
+            />
           </Switch>
         </Router>
         <Modal />

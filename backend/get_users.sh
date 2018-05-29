436a7a2747d3e44da9340320d33a429f96ec9ba2
Oliver (T560) <olli.bienert@gmail.com>
2018-05-29 18:42:23 +0200

    Backend: get_users: Remove exports of environment variables (Use .profile instead)

--- backend/get_users.sh
+++ backend/get_users.sh
@@ -2,8 +2,5 @@
 
 exec 1> >(logger -s -t $(basename $0)) 2>&1
 
-export FLASK_APP=/home/openschufa/sites/openschufa/backend/main.py
-export SECRET_KEY=secret_key
-export OPENSCHUFA_DATABASE_URL=postgresql://openschufa:openschufa@localhost/openschufa
 source /home/openschufa/.venvs/openschufa/bin/activate
 flask get_users
